7f097952364c446b2816b722e4959b050654fd8a
Jack Pflaum <jackpflaum@gmail.com>
2023-05-14 15:25:16 +0800

    add Project model to admin

--- MyPortfolioWebsite/jacks_website/admin.py
+++ MyPortfolioWebsite/jacks_website/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+#decorator directly registers model class Project with ProjectAdmin without calling admin.site.register() function separately.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    pass

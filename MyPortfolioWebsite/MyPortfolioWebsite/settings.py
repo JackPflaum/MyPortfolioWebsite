9e018de43180ed09f43760fb815328257a7795e2
Jack Pflaum <jackpflaum@gmail.com>
2023-05-14 15:39:02 +0800

    add app to INSTALLED_APPS, fixed find_dotenv() and and fixed main urls app name

--- MyPortfolioWebsite/MyPortfolioWebsite/settings.py
+++ MyPortfolioWebsite/MyPortfolioWebsite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-load_dotenv(find_dotenv)
+load_dotenv(find_dotenv())
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jacks_website',
 ]
 
 MIDDLEWARE = [

7b50665f17863b8fee7f0a193583cbd550ee8fd3
Jack Pflaum <jackpflaum@gmail.com>
2023-08-28 15:55:26 +0800

    created 'ProjectImage' model to hold multiple images for the same project.

--- MyPortfolioWebsite/jacks_website/models.py
+++ MyPortfolioWebsite/jacks_website/models.py
@@ -3,12 +3,12 @@ from django.utils.text import slugify
 
 
 class Project(models.Model):
+    """Details for each project"""
     title = models.CharField(max_length=255)
     brief_description = models.CharField(max_length=255, null=True)
     details = models.TextField()
     website_url = models.URLField(blank=True)
     github_url = models.URLField(blank=True)
-    image = models.ImageField(upload_to='images/')
     video = models.FileField(upload_to='videos/', null=True, blank=True)
     slug = models.SlugField(unique=True, max_length=255, null=True)
 
@@ -21,3 +21,9 @@ class Project(models.Model):
     def save(self, *args, **kwargs):
         self.slugify_title()
         super().save(*args, **kwargs)
+
+
+class ProjectImage(models.Model):
+    """allows me to store multiple images for the same project"""
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='images/')

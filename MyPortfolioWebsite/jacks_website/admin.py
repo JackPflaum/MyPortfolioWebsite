01d4571e2c183fde8a664e3eba1df558d75ac745
Jack Pflaum <jackpflaum@gmail.com>
2023-08-28 15:55:49 +0800

    registered 'ProjectImage' model to Django admin

--- MyPortfolioWebsite/jacks_website/admin.py
+++ MyPortfolioWebsite/jacks_website/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, ProjectImage
 
 #decorator directly registers model class Project with ProjectAdmin without calling admin.site.register() function separately.
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     pass
+
+@admin.register(ProjectImage)
+class ProjectImageAdmin(admin.ModelAdmin):
+    list_display = ['project', 'image']

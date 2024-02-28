9db03e2d247fa949e52a275a0e988983706841a5
Jack Pflaum <jackpflaum@gmail.com>
2024-02-28 20:34:43 +0800

    moved files for deployment purposes on Render.com

--- MyPortfolioWebsite/build.sh
+++ MyPortfolioWebsite/build.sh
@@ -5,8 +5,5 @@ set -o errexit
 # install dependencies
 pip install -r requirements.txt
 
-# navigate to folder containing manage.py file
-cd MyPortfolioWebsite
-
 python manage.py collectstatic --no-input
 python manage.py migrate
